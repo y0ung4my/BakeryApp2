3e04c1a49b74412edde74206cc6ff012ca28345d
Amy Young <youngamy1223@gmail.com>
2022-04-09 13:00:08 -0600

    add Models

---
+++ Bakery/Models/BakeryContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Bakery.Models
+{
+  public class BakeryContext : DbContext
+  {
+    public DbSet<Flavor> Flavors { get; set; }
+    public DbSet<Treat> Treats { get; set; }
+    public DbSet<FlavorTreat> FlavorTreat { get; set; }
+
+    public BakeryContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
+  }
+}
