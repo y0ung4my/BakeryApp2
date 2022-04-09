3dd06ff3ce24da2747e4a58de293214bafbf5feb
Amy Young <youngamy1223@gmail.com>
2022-04-09 14:08:05 -0600

    add flavors index

---
+++ Bakery/Controllers/FlavorsController.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Bakery.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bakery.Controllers
+{
+  public class FlavorsController : Controller
+  {
+    private readonly BakeryContext _db;
+
+    public FlavorsController(BakeryContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Flavor> model = _db.Flavors.ToList();
+      return View(model);
+    }
+
+  }
+    
+}
